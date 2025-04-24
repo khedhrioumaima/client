0831d78845172bd753b6fddf64b539fd074cbf05
unknown <khedhrioumaima181@gmail.com>
2025-04-24 11:36:45 +0100

    adding files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port : 5173}
+  server: {port : 5173},
+  // vite.config.js
+  build: {
+    outDir: 'build', // Force Vite to output to 'build/'
+  }
 })
+
+
